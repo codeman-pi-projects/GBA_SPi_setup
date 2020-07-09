f60c84015dc700d8811a97963dee8141ae9fb6ef
codeman-pi-projects <40361046+codeman-pi-projects@users.noreply.github.com>
2020-07-09 10:31:51 -0400

    Update GBA_SPiStartup.sh

--- GBA_SPi/GBA_SPiStartup.sh
+++ GBA_SPi/GBA_SPiStartup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-omxplayer -o alsa -b --layer 10000 "/home/pi/GBA_SPi_splash_v1.mp4" &
+#omxplayer -o alsa -b --layer 10000 "/home/pi/GBA_SPi_splash_v1.mp4" &
 sudo python /home/pi/Button_Controller/monitor.py
-sleep 25s
-sudo pkill omxplayer
+#sleep 25s
+#sudo pkill omxplayer
